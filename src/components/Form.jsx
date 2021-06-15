9e810e61dbf1f5d00ed518b566e2a3e1daf20c67
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-15 13:34:09 -0400

    changed confirm password to not show up instead of replacing it with empty string.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,9 +5,7 @@ function Form(props) {
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      {props.isRegistered ? (
-        ""
-      ) : (
+      {props.isRegistered === false && (
         <input type="password" placeholder="Confirm Password" />
       )}
       <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
